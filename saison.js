489961ce745d16a7b5623cf4ebc9b7bd8668f050
solene Verrier <sosso711@hotmail.com>
2021-09-30 17:04:32 +0200

    start js saison

---
+++ saison.js
@@ -0,0 +1,32 @@
+/*const imageEvent=document.querySelector ('.img1')
+const image =document.createElement('img')
+image.src = '../images/index.jpeg';
+
+imageEvent.appendChild(image);
+image.style.width= '500px';
+
+
+const imageEvent2 = document.querySelector ('.img2');
+const image2 = document.createElement ('img');
+image.src = 'images/images.jpeg ';
+
+imageEvent2.appendChild (image);*/
+
+
+const Biennale = {tittleEvent : 'biennale de la Danse', image : img.src=images/images.jpeg, textEvent :'tototot'}
+function createEvent (tittleEvent, image,textEvent) {
+    return (Biennale)}
+
+ /*const image = document.createElement("img");
+image.classList.add("image");
+image.appendChild(image);
+
+    const tittleEvent = document.createElement("tittle");
+   tittle.classList.add("tittle");
+    tttleEvent.appendChild(tittle);
+  
+    const textEvent = document.createElement("p");
+    textEvent.classList.add("p");
+    textEvent.appendChild(p);}
+  
+  console.log (createEvent)
