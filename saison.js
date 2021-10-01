09787c14313c08f8022c2c116c86bb598d2a7366
solene Verrier <sosso711@hotmail.com>
2021-10-01 14:24:37 +0200

    add img2 js

--- saison.js
+++ saison.js
@@ -13,25 +13,42 @@ image2.src = 'images/images.jpeg ';
 imageEvent2.appendChild (image2);*/
 
 
-const biennale = {tittleEvent : 'biennale de la Danse', image : './images/index.jpeg', textEvent :'tototot'}
-function createEvent ( tittleEvent, imageUrl,textEvent) {
-  const imageEvent=document.querySelector ('.img1')
-  const image =document.createElement('img')
-  image.src = imageUrl;
-  imageEvent.appendChild(image);
-image.style.width= '500px';
-  }
-createEvent(biennale.tittleEvent, biennale.image, biennale.textEvent)
-
-
-
-
-    /*const tittleEvent = document.createElement("tittle");
+const biennale = {tittle : 'biennale de la Danse', image1 : './images/index.jpeg' ,image2: './images/images.jpeg' ,textEvent :'tototot'}
+function createEvent ( tittle, imageUrl,imageUrl2, textEvent) {
+  const imageEvent=document.querySelector ('.img1');
+  const image1 =document.createElement('img');
+  image1.src = imageUrl;
+  image1.style.width='100%';
+  image1.style.height = 'auto';
+  imageEvent.appendChild(image1);
   
- tttleEvent.appendChild(tittle);
+  const imageEvent2=document.querySelector ('.img2');
+  const image2 =document.createElement('img');
+  image2.src = imageUrl2;
+  image2.style.width='100%';
+  image2.style.height = 'auto';;
+  imageEvent2.appendChild(image2); 
   
-    const textEvent = document.createElement("p");
-    textEvent.classList.add("p");
-    textEvent.appendChild(p);}
   
-  console.log (createEvent)*/
+  /*const tittleEvent = document.createElement(".tittle");
+  const tittle = document.createElement("h2");
+  tittle.style.color='red';
+    
+ tittleEvent.appendChild(tittle);*/
+  
+    
+  
+
+}
+createEvent(biennale.tittle, biennale.image1, biennale.image2, biennale.textEvent)
+
+
+  
+
+
+
+  
+
+
+
+
