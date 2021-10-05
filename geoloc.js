08be00ef8085c932f8d5b97881e84bb37302acfc
NycoChavanoud <nicolas.chavanoud@gmail.com>
2021-10-05 16:51:50 +0200

    create markers when input adress && asave in local storage

--- geoloc.js
+++ geoloc.js
@@ -1,5 +1,23 @@
 const locatBtn = document.querySelector('#geoloc-button');
 const geolocStatus = document.querySelector('#geoloc-status');
+let eventListInLocalStorage = JSON.parse(localStorage.getItem('eventList'));
+
+if (eventListInLocalStorage === null) {
+  eventListInLocalStorage = [
+    {
+      name: 'concert - hall tony garnier',
+      coords: { lat: 45.731904, lng: 4.825906 },
+    },
+    {
+      name: 'vogue des Marrons - Bvd Croix-rousse',
+      coords: { lat: 45.773642, lng: 4.823392 },
+    },
+    {
+      name: 'Place Bellecour',
+      coords: { lat: 45.757456, lng: 4.832211 },
+    },
+  ];
+}
 
 let map = L.map('mapid').setView([45.74563313490728, 2.827675010610884], 15);
 
@@ -8,34 +26,22 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
 
-const markerHallTonyGarnier = L.marker([45.731904, 4.825906])
-  .addTo(map)
-  .bindPopup('Hall Tony Garnier');
-
-const placeBellecour = L.marker([45.757456, 4.832211])
-  .addTo(map)
-  .bindPopup('Place Bellecour');
-
-const boulevardCroixRousse = L.marker([45.773642, 4.823392])
-  .addTo(map)
-  .bindPopup('Boulevard de la Croix-Rousse');
-
-map.fitBounds([
-  [45.757456, 4.832211],
-  [45.731904, 4.825906],
-  [45.773642, 4.823392],
-]);
-
-
+for (let i = 0; i < eventListInLocalStorage.length; i++) {
+  const currentEvent = eventListInLocalStorage[i];
+  console.log(currentEvent);
+  L.marker([currentEvent.coords.lat, currentEvent.coords.lng])
+    .addTo(map)
+    .bindPopup(currentEvent.name);
+}
 
 function onGeolocationSuccess(position) {
   console.log(position);
   const lat = position.coords.latitude;
-  const long = position.coords.longitude;
+  const lng = position.coords.longitude;
 
-  map.setView([lat, long], 15);
+  map.setView([lat, lng], 15);
 
-  L.marker([lat, long]).addTo(map).bindPopup('Vous êtes ici!').openPopup();
+  L.marker([lat, lng]).addTo(map).bindPopup('Vous êtes ici!').openPopup();
 }
 
 function onGeolocationError(error) {
@@ -55,6 +61,51 @@ locatBtn.addEventListener('click', function () {
   }
 });
 
-//* flist marker sur map//*
+//* fonction transformation d'adresse en lat/long//*
 
-//* fonction initéraire //*
+const API_KEY = '2f9c8efda6a24f89b0dab37cf6865bb8';
+
+function getCoords(addr) {
+  const formattedURI = encodeURI(
+    `https://api.opencagedata.com/geocode/v1/json?q=${addr}&key=${API_KEY}&language=fr&pretty=1`
+  );
+  return fetch(formattedURI)
+    .then((res) => res.json())
+    .then((response) => response?.results[0]?.geometry || null)
+    .catch((error) => {
+      console.error(error);
+      return null;
+    });
+}
+
+const eventNameInput = document.querySelector('#name');
+const eventAddrInput = document.querySelector('#addr');
+const formEvent = document.querySelector('#formGeoloc');
+
+formEvent.addEventListener('submit', function (event) {
+  event.preventDefault();
+  const newEvent = { name: eventNameInput.value, addr: eventAddrInput.value };
+
+  getCoords(newEvent.addr).then((coords) => {
+    if (coords) {
+      newEvent.coords = coords;
+      eventListInLocalStorage.push(newEvent);
+
+      localStorage.setItem(
+        'eventList',
+        JSON.stringify(eventListInLocalStorage)
+      );
+    } else {
+      console.log('no result found, try another search');
+    }
+  });
+});
+
+// // Echelle de l'affichage de la carte !!
+// A adapter en fonction des éléments, après le merge avec le reste!!
+
+map.fitBounds([
+  [45.757456, 4.832211],
+  [45.731904, 4.825906],
+  [45.773642, 4.823392],
+]);
